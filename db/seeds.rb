ed263bb146ae77c60880d85f03071565b343f3cb
Niall <noccer@yahoo.ie>
2016-07-11 20:37:52 +1000

    NOC: Seed file. Adjustments will be needed but the data structure is now complete.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,47 @@
 #
 #   cities = City.create([[ name: 'Chicago' ], [ name: 'Copenhagen' ]])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Conference.delete_all
+Project.delete_all
 Presenter.delete_all
+
+Conference.create([
+  {
+    conference_title: "WDI7 Conf - Serene Windcaves",
+    conference_address: "45 William Street",
+    conference_suburb: "Melbourne",
+    conference_postcode: 3000,
+    conference_username: "jammin",
+    password: "123"
+  }
+])
+
+Project.create([
+  {
+    project_title: "Pulse Express",
+    project_description: "A live music app... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum accumsan ex sed accumsan. Ut urna elit, finibus pharetra bibendum et, elementum et erat. Curabitur porttitor mauris tellus. Donec facilisis leo a fermentum consequat. Vivamus pharetra, leo vel tincidunt tempor, ipsum lorem scelerisque nisi, quis aliquam mi neque pulvinar nisl.",
+    project_time: DateTime.new(2016,7,17,10),
+    project_image1: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1000px-Google_%22G%22_Logo.svg.png",
+    project_image2: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1000px-Google_%22G%22_Logo.svg.png",
+    project_image3: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1000px-Google_%22G%22_Logo.svg.png",
+    project_github: "https://github.com/ctrlaltchris/pulse-express-front",
+    conference_id: Conference.all[0].id
+  },
+  {
+    project_title: "Queuer",
+    project_description: "A queue cure... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum accumsan ex sed accumsan. Ut urna elit, finibus pharetra bibendum et, elementum et erat. Curabitur porttitor mauris tellus. Donec facilisis leo a fermentum consequat. Vivamus pharetra, leo vel tincidunt tempor, ipsum lorem scelerisque nisi, quis aliquam mi neque pulvinar nisl.",
+    project_time: DateTime.new(2016,7,17,14),
+    project_image1: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1000px-Google_%22G%22_Logo.svg.png",
+    project_image2: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1000px-Google_%22G%22_Logo.svg.png",
+    project_image3: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1000px-Google_%22G%22_Logo.svg.png",
+    project_github: "https://github.com/jasonlow90/restaurant-waitlist-v2",
+    conference_id: Conference.all[0].id
+  },
+])
+
 Presenter.create([
-  [
+  {
     presenter_name: "Niall O'Callaghan",
     presenter_title: "Full Stack Web Developer",
     presenter_image: "app/assets/images/niall.JPG",
@@ -23,10 +61,11 @@ Presenter.create([
       [ "Linux" , "<i class='devicon-linux-plain'></i>" ],
       [ "Gimp" , "<i class='devicon-gimp-plain'></i>" ]
     ],
-    social_media: "https://au.linkedin.com/in/niall-o-callaghan-webdev",
-    presenter_github: "https://github.com/noccer2016"
-  ],
-  [
+    presenter_social: "https://au.linkedin.com/in/niall-o-callaghan-webdev",
+    presenter_github: "https://github.com/noccer2016",
+    project_id: Project.all[1].id
+  },
+  {
     presenter_name: "Peter Daniel",
     presenter_title: "Frontend Web Developer",
     presenter_image: "app/assets/images/peter.JPG",
@@ -40,10 +79,11 @@ Presenter.create([
       [ "AngularJS" , "<i class='devicon-angularjs-plain'></i>" ],
       [ "Photoshop" , "<i class='devicon-photoshop-plain'></i>" ]
     ],
-    social_media: "https://au.linkedin.com/in/peterdaviddaniel",
-    presenter_github: "https://github.com/peterdaniel-fewd"
-  ],
-  [
+    presenter_social: "https://au.linkedin.com/in/peterdaviddaniel",
+    presenter_github: "https://github.com/peterdaniel-fewd",
+    project_id: Project.all[1].id
+  },
+  {
     presenter_name: "Chris McCorry",
     presenter_title: "Front End Web Developer",
     presenter_image: "app/assets/images/cjm.JPG",
@@ -58,10 +98,11 @@ Presenter.create([
       [ "WordPress" , "<i class='devicon-wordpress-plain'></i>" ],
       [ "Photoshop" , "<i class='devicon-photoshop-plain'></i>" ]
     ],
-    social_media: "https://au.linkedin.com/in/chrismccorry",
-    presenter_github: "https://github.com/ctrlaltchris"
-  ],
-  [
+    presenter_social: "https://au.linkedin.com/in/chrismccorry",
+    presenter_github: "https://github.com/ctrlaltchris",
+    project_id: Project.all[0].id
+  },
+  {
     presenter_name: "Jason Low",
     presenter_title: "Full Stack Web Developer",
     presenter_image: "app/assets/images/IMG_0336.JPG",
@@ -74,10 +115,11 @@ Presenter.create([
       [ "Ruby on Rails" , "<i class='devicon-rails-plain'></i>" ],
       [ "AngularJS" , "<i class='devicon-angularjs-plain'></i>" ],
     ],
-    social_media: "https://au.linkedin.com/in/jason-low-72998899",
-    presenter_github: "https://github.com/jasonlow90"
-  ],
-  [
+    presenter_social: "https://au.linkedin.com/in/jason-low-72998899",
+    presenter_github: "https://github.com/jasonlow90",
+    project_id: Project.all[1].id
+  },
+  {
     presenter_name: "Joel Brewster",
     presenter_title: "Full Stack Web Developer",
     presenter_image: "app/assets/images/joelbrewster.jpg",
@@ -92,10 +134,11 @@ Presenter.create([
       [ "Linux" , "<i class='devicon-linux-plain'></i>" ],
       [ "Vim" , "<i class='devicon-vim-plain'></i>" ]
     ],
-    social_media: "https://au.linkedin.com/in/joelbrewster",
-    presenter_github: "https://github.com/joelbrewster"
-  ],
-  [
+    presenter_social: "https://au.linkedin.com/in/joelbrewster",
+    presenter_github: "https://github.com/joelbrewster",
+    project_id: Project.all[0].id
+  },
+  {
     presenter_name: "Will Guan",
     presenter_title: "Full Stack Web Developer",
     presenter_image: "app/assets/images/willg.jpg",
@@ -108,7 +151,8 @@ Presenter.create([
       [ "Ruby on Rails" , "<i class='devicon-rails-plain'></i>" ],
       [ "AngularJS" , "<i class='devicon-angularjs-plain'></i>" ],
     ],
-    social_media: "https://au.linkedin.com/in/guanwill",
-    presenter_github: "https://github.com/guanwill"
-  ]
+    presenter_social: "https://au.linkedin.com/in/guanwill",
+    presenter_github: "https://github.com/guanwill",
+    project_id: Project.all[0].id
+  }
 ])
