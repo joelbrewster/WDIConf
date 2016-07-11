fa255fba12a0ab37667a49abfc4a3626ad1ed7b4
Niall <noccer@yahoo.ie>
2016-07-11 18:20:27 +1000

    NOC: ran 'annotate' to insert schema information. Added some references to the model: belongs_to conference has_many presenters

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id                  :integer          not null, primary key
+#  project_title       :string
+#  project_description :string
+#  project_time        :datetime
+#  project_image1      :string
+#  project_image2      :string
+#  project_image3      :string
+#  project_github      :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  conference_id       :integer
+#
+
 class Project < ActiveRecord::Base
+  belongs_to :conference
+  has_many :presenters
 end
