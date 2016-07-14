34a9db5bfc32616e65f289d0331af30d9a5edb17
Niall <noccer@yahoo.ie>
2016-07-14 21:49:39 +1000

    NOC: fresh running of 'annotate' to update the models. Added in some new methods for Presenter CRUD

--- app/models/presenter.rb
+++ app/models/presenter.rb
@@ -6,14 +6,35 @@
 #  presenter_name   :string
 #  presenter_title  :string
 #  presenter_image  :string
-#  presenter_skills :string
 #  presenter_social :string
 #  presenter_github :string
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #  project_id       :integer
+#  presenter_skills :text             default([]), is an Array
 #
 
 class Presenter < ActiveRecord::Base
   belongs_to :project
+
+  def index
+    @presenters = Presenter.all
+  end
+
+  def create_presenter
+    presenter = Presenter.new
+    presnter.presenter_name = params(:presenter_name)
+  end
+
+  def edit_presenter
+
+  end
+
+  def update_presenter
+
+  end
+
+  def delete_presenter
+
+  end
 end
