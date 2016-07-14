b5df16e6b31ca2033331f68edae76cd41f32bba8
Niall <noccer@yahoo.ie>
2016-07-14 22:43:32 +1000

    NOC: Moved controller logic... to the controller! What was I thinking?! Need some ZzZz's...

--- app/models/presenter.rb
+++ app/models/presenter.rb
@@ -16,25 +16,4 @@
 
 class Presenter < ActiveRecord::Base
   belongs_to :project
-
-  def index
-    @presenters = Presenter.all
-  end
-
-  def create_presenter
-    presenter = Presenter.new
-    presnter.presenter_name = params(:presenter_name)
-  end
-
-  def edit_presenter
-
-  end
-
-  def update_presenter
-
-  end
-
-  def delete_presenter
-
-  end
 end
